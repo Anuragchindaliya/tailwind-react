5ea7e1f2e143f03e7b67d57b84a3b13f943fdf4f
Anuragchindaliya <ak1148383@gmail.com>
2022-03-05 18:56:48 +0530

    Add:Tailwind components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Content from "./components/Content";
+import Cta from "./components/Cta";
+import Features from "./components/Features";
+import Footer from "./components/Footer";
+import Gallery from "./components/Gallery";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Statistic from "./components/Statistic";
+import Steps from "./components/Steps";
+import Testimonials from "./components/Testimonials";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Hero />
+      <Features />
+      <Content />
+      <Statistic />
+      <Steps />
+      <Testimonials />
+      <Gallery />
+      <Cta />
+      <Footer />
+    </>
   );
 }
 
